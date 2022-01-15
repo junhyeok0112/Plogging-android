7bfef27323979c7b78fed2188d2a4bab741f7dc4
junhyeok0112 <wd78wg@naver.com>
2022-01-16 04:49:57 +0900

    HomeFragment v1

---
+++ app/src/main/java/org/techtown/plogging_android/HomeFragment.kt
@@ -0,0 +1,46 @@
+package org.techtown.plogging_android
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import org.techtown.plogging_android.Dacorator.SaturdayDecorator
+import org.techtown.plogging_android.Dacorator.SundayDecorator
+import org.techtown.plogging_android.Dacorator.TodayDecorator
+import org.techtown.plogging_android.databinding.FragmentHomeBinding
+import java.util.*
+
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding:FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    fun setCalendar(){
+
+        var startTimeCalendar = Calendar.getInstance()
+        var endTimeCalendar = Calendar.getInstance()
+
+        val currentYear = startTimeCalendar.get(Calendar.YEAR)
+        val currentMonth = startTimeCalendar.get(Calendar.MONTH)
+        val currentDate = startTimeCalendar.get(Calendar.DATE)
+
+        val stCalendarDay = CalendarDay(currentYear, currentMonth, currentDate)
+        val enCalendarDay = CalendarDay(endTimeCalendar.get(Calendar.YEAR), endTimeCalendar.get(Calendar.MONTH), endTimeCalendar.get(Calendar.DATE))
+
+        val sundayDecorator = SundayDecorator()
+        val saturdayDecorator = SaturdayDecorator()
+        val todayDecorator = TodayDecorator(requireContext())
+
+        //binding.homeCalnedarCv.addDecorators(sundayDecorator, saturdayDecorator,todayDecorator)
+    }
+}
--- app/src/main/java/org/techtown/plogging_android/MainActivity.kt
+++ app/src/main/java/org/techtown/plogging_android/MainActivity.kt
@@ -2,10 +2,18 @@ package org.techtown.plogging_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.techtown.plogging_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.main_container_fl , HomeFragment())
+            .commit()
     }
 }
